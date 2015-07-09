4439a6fdb5497165c35e9e27126faea0c28e21c5
urandu <bildadnamawa@gmail.com>
2015-07-09 14:39:43 +0300

    Project skeleton set-up

---
+++ create_user.sql
@@ -0,0 +1,9 @@
+-- Create impact user
+CREATE USER 'msh'@'localhost' IDENTIFIED BY 'msh>otero12345625lll>?>>';
+-- Grant permissions to impact user
+GRANT INSERT ON msh_db.* TO 'msh'@'localhost';
+GRANT DELETE ON msh_db.* TO 'msh'@'localhost';
+GRANT UPDATE ON msh_db.* TO 'msh'@'localhost';
+GRANT SELECT ON msh_db.* TO 'msh'@'localhost';
+-- Reload permissions 
+FLUSH PRIVILEGES;
