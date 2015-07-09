ef36c3c1249256cee940ed416346b32e60e3c8eb
urandu <bildadnamawa@gmail.com>
2015-07-09 14:41:13 +0300

    database settings configured

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'msh';
+$db['default']['password'] = 'msh>otero12345625lll>?>>';
+$db['default']['database'] = 'msh';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
