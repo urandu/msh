7a45c5788190a3be90eb3af16859c3b3d162c665
urandu <bildadnamawa@gmail.com>
2015-07-11 16:13:58 +0300

    Register controller & view added

---
+++ application/views/includes/bottom_includes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: urandu
+ * Date: 7/11/15
+ * Time: 1:38 PM
+ */
+?>
+
+<!-- Mainly scripts -->
+<script src="<?php echo(base_url()); ?>assets/js/jquery-2.1.1.js"></script>
+<script src="<?php echo(base_url()); ?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo(base_url()); ?>assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="<?php echo(base_url()); ?>assets/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="<?php echo(base_url()); ?>assets/js/inspinia.js"></script>
+<script src="<?php echo(base_url()); ?>assets/js/plugins/pace/pace.min.js"></script>
+
+
+
+</body>
+
+</html>
