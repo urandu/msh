41fdbd4e9271722ff673e47aed67b543249b7835
urandu <bildadnamawa@gmail.com>
2015-07-12 13:25:02 +0300

    Navigation completed

--- application/views/facility_mos.php
+++ application/views/facility_mos.php
@@ -3,7 +3,7 @@
 
     <div class="row wrapper border-bottom white-bg page-heading">
         <div class="col-sm-4">
-            <h2>Dashboard</h2>
+            <h2>Facility level MOS</h2>
             <!--<ol class="breadcrumb">
                 <li>
                     <a href="index.html">This is</a>
