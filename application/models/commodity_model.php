33bce18f1b9efdc241c962748e2d6d8adb631d75
Ndlovu <patrickngachuru@gmail.com>
2015-07-12 15:51:10 +0300

    agency and commodity models

--- application/models/commodity_model.php
+++ application/models/commodity_model.php
@@ -1,15 +1,13 @@
-<?php 
-
+<?php
 	// GET THE COMMODITY WITH NAME SAME AS THE NAME IN CENTRAL DATA
-	function get_commodity_id_with_the_given_name($comm_name){
+	function get_commodity_id_with_the_given_name($commodity_name){
 	$this->db->select('commodity_id');
 	$this->db->from('commodities');
-	$this->db->where('commodity_name', $comm_name);
+	$this->db->where('commodity_name', $commodity_name);
 	$query = $this->db->get();
 	$result = $query->row()->commodity_id;
 	return $result;
 	}
-
 	// Function To Fetch All Commodies Record
 	function show_commodities(){
 	$query = $this->db->get('commodities');
@@ -25,19 +23,18 @@
 	$result = $query->result();
 	return $result;
 	}
-
 	// Update Query For Selected Student
 	function update_commodity_id($id,$data){
-	    $this->db->where('commodity_id', $id);
-	    $this->db->update('commodities', $data);
+	$this->db->where('commodity_id', $id);
+	$this->db->update('commodities', $data);
 	}
 
-
+	/*this function adds a commodity to the database*/
 	function addcommodity($commodity=NULL){		
 	$this->db->insert('commodities', $commodity);
 	return $this->db->insert_id();						
 	}
-
+	/*this function gets all commodities in the database*/
 	function getCommodity(){	
 		$this->db->select('*');
 		$this->db->from('commodities');				
