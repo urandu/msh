a89720e1e617bd35959b83d8e0a26b7ba71cc7d4
Enock Oloo <enockoloo6@gmail.com>
2015-07-13 18:37:19 +0300

    supply chain complete

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/msh';
+$config['base_url'] = 'http://localhost/msh/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/supply_chain.php
+++ application/controllers/supply_chain.php
@@ -17,9 +17,97 @@ class Supply_chain extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('agency_model');
+
+	}
+
 	public function index()
 	{
-		$this->load->view('supply_chain');
+
+		$this->show_agency_id();
+
+	}
+
+
+	public function show_agency_id() {
+		$id = $this->uri->segment(3);//get id from the url
+		$data['agencies'] = $this->agency_model->show_supply_chain_agencies();
+		$data['single_agency'] = $this->agency_model->show_supply_chain_agency_id($id);
+
+		$this->load->view('supply_chain',$data);
+	}
+
+	function update_agency_id1() {
+		$id= $this->input->post('supply_agency_id');
+		$data = array(
+			'SUPPLY_CHAIN_AGENCY' => $this->input->post('supply_agency_name'),
+			'CONTACT_PERSON' => $this->input->post('contact_person'),
+			'CONTACT_PHONE' => $this->input->post('contact_phone'),
+			'COMMENT' => $this->input->post('supply_agency_description'),
+
+		);
+		$updaterecord=$this->agency_model->update_supply_chain_agency_id1($id,$data);
+
+		$data['formupdate'] =  "";
+		if($updaterecord){
+			$data['formupdate'] =  "set";
+		}
+
+
+		$id = $this->uri->segment(3);//get id from the url
+		$data['agencies'] = $this->agency_model->show_supply_chain_agencies();
+		$data['single_agency'] = $this->agency_model->show_supply_chain_agency_id($id);
+
+
+		$this->load->view('supply_chain',$data);
+
+		// $this->show_agency_id();
+	}
+
+	function delete_supply_chain_agency($id)   {
+
+		$this->db->where('supply_chain_agency_id', $id);
+		$deleterecord=$this->db->delete('supply_chain_agencies');
+		$data['status'] =  "";
+		if($deleterecord){
+			$data['status'] =  "Agency deleted Successfully!..";
+		}
+
+		$this->show_agency_id();
+	}
+
+
+
+
+
+	public function save_supply_chain_agency()
+	{
+		$agency_name = ($this->input->post('supply_agency_name'));
+		$person = ($this->input->post('contact_person'));
+		$contact = ($this->input->post('contact_phone'));
+		$phone = ($this->input->post('supply_chain_description'));
+
+
+		$agency = array(
+			'SUPPLY_CHAIN_AGENCY' => $agency_name,
+			'CONTACT_PERSON' => $person,
+			'CONTACT_PHONE' => $contact,
+			'COMMENT' => $phone
+		);
+
+		$agencyId = $this->agency_model->add_supply_chain_agency($agency);
+		$data['message'] = "";
+		if ($agencyId) {
+			$data['message'] = "Agency Saved Successfully!..";
+		}
+
+		redirect(base_url()."supply_chain");
+
 	}
 }
 
