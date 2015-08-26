99cd7740a9bc9c4f2ac1f90ecb5277a8d043902e
banga <dennisbanga@gmail.com>
2015-08-26 10:51:32 +0300

    updated the county level reporting rates script url with live hiskenya url

--- cronjob_tasks/county_level_reporting_rates.php
+++ cronjob_tasks/county_level_reporting_rates.php
@@ -1,10 +1,7 @@
 <?php
-include('connect.php');
-include('fetch.php');
-include('county_level_reporting_rates_year.php');
-//API login Credentials
-$username="Bootcamp";
-$password="Bootcamp2015";
+require 'connect.php';
+require 'fetch.php';
+require 'county_level_reporting_rates_year.php';
 
 $html=json_decode($period_json);
 function add_date($date,$interval)
@@ -26,59 +23,54 @@ $array_length=count($html);
     for($i=0;$i<$array_length;$i++)
     {
 
-$url="http://test.hiskenya.org/api/analytics?dimension=dx:JPaviRmSsJW&dimension=pe:LAST_12_MONTHS&filter=ou:".$html[$i]."&displayProperty=NAME";
-// $url_orgUnit="http://test.hiskenya.org/api/organisationUnits";
+        $url="https://hiskenya.org/api/analytics?dimension=dx:JPaviRmSsJW&dimension=pe:LAST_12_MONTHS&filter=ou:".$html[$i]."&displayProperty=NAME";
 
-// $data = array("dataSet" => "$dataset", "period" => "$period", "orgUnit" => "$orgUnit");
-// $data_string = http_build_query($data);
-// $url.="$data_string";
+        // initailizing curl
+        $ch = curl_init();
+        //curl options
+        curl_setopt($ch, CURLOPT_POST, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+        //execute
+        $result = curl_exec($ch);
 
-// initailizing curl
-$ch = curl_init();
-//curl options
-curl_setopt($ch, CURLOPT_POST, false);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-//execute
-$result = curl_exec($ch);
+        //close connection
+        curl_close($ch);
+        if ($result){
+            
+        	$result=json_decode($result,true);
+        	$json=$result['rows'];
+        	// print_r($json);
 
-//close connection
-curl_close($ch);
-if ($result){
+        	 foreach($json as $row)
+            { 
+               if($row[1]==$last_month){
 
-	$result=json_decode($result,true);
-	$json=$result['rows'];
-	//print_r($json);
+                    $periodic = $row[1];
+                    $reportvalue = $row[2];
 
-	 foreach($json as $row)
-    { 
-       if($row[1]==$last_month)
-     {
-    $periodic = $row[1];
-    $reportvalue = $row[2];
+                   $sql = "INSERT INTO county_level_reporting_rates(reporting_rate_value,period,county_id)VALUES('$reportvalue','$periodic','$html[$i]')";
+                    if(mysql_query($sql,$con))
+                    {
+                    	echo ("inserted successfully <br>");
+                        
+                    }else
+                    {
+                       die('Error : ' . mysql_error()); 
+                    }
+                }
+            }
+        }
+        else{
+            echo -1;
+        }
 
-   $sql = "INSERT INTO county_reporting_rates(reporting_rate_value,period,county_id)VALUES('$reportvalue','$periodic','$html[$i]')";
-    if(mysql_query($sql,$con))
-    {
-    	echo ("inserted successfully <br>");
-        
-    }else
-       die('Error : ' . mysql_error()); 
-    }
-
-}
-}
-
-else{
-
-    echo -1;
-}
 }
 ?>
 
